be6d088709f5e1f457fc5d654c89c3f89a2335f1
aliciangel88 <alpabebe1@gmail.com>
2023-09-23 23:36:48 -0600

    razor and annotations

---
+++ MVCRazorDemo/Models/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCRazorDemo.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+
+        [Index(IsUnique = true)]
+        [ConcurrencyCheck]
+        [MaxLength(24), MinLength(5)]
+        public string Title { get; set; }
+        [Index]
+        public int Credits { get; set; }
+
+        [Timestamp, DataType("timestamp")]
+        public byte[] TimeStamp { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        [InverseProperty("CurrCourse")]
+        public virtual ICollection<Enrollment> CurrEnrollments { get; set; }
+
+        [InverseProperty("PrevCourse")]
+        public virtual ICollection<Enrollment> PrevEnrollments { get; set; }
+
+
+    }
+}
---
+++ MVCRazorDemo/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCRazorDemo.Models
+{
+    [Table("StudentsInfo", Schema = "Admin")]
+    public class Student
+    {
+        [Key]
+        public int StdntID { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Column("FirstName")]
+        [Required]
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        [NotMapped]
+        public int FatherName { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
