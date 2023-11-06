70fad693a341a08fc14c681ebfdbb2d9ae60c993
aliciangel88 <alpabebe1@gmail.com>
2023-11-05 22:21:31 -0600

    WebFormsIdentity

---
+++ WebFormsIdentity/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(WebFormsIdentity.Startup))]
+
+namespace WebFormsIdentity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login")
+            });
+        }
+    }
+}
